4f1a920ff79780bb6320bd2b51df1dbe71ef5c4f
Sebastian Ibarguen <sebasibarguen@gmail.com>
2017-10-05 18:03:10 -0600

    added basic script. added validation and test datasets.

--- projects/scene-classification/setup.sh
+++ projects/scene-classification/setup.sh
@@ -1,2 +1,5 @@
 wget -P data http://ai-challenger-scene.ufile.ucloud.com.cn/ai_challenger_scene_train_20170904.zip
-cd data && unzip ai_challenger_scene_train_20170904.zip
+wget -P data http://static1.challenger.ai/ai_challenger_scene_validation_20170908.zip?OSSAccessKeyId=LTAIrOA0FFfXPB7A&Expires=1509840018&Signature=2eSHeDBIxXf76yBXi9psqY5J%2Bz4%3D
+wget -P data http://static1.challenger.ai/ai_challenger_scene_test_a_20170922.zip?OSSAccessKeyId=LTAIrOA0FFfXPB7A&Expires=1509840023&Signature=WiAVhlSOeRX08olC2tgSUlsqvYI%3D
+
+cd data && unzip ai_challenger_scene_train_20170904.zip && unzip ai_challenger_scene_validation_20170908.zip && unzip ai_challenger_scene_test_a_20170922.zip
